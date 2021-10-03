29f03d6181470dac1434a2b18d214d030c89c464
sehaj97 <sehajmagan@gmail.com>
2021-10-02 22:14:16 -0400

    error handling

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -16,19 +16,35 @@ var getUserRepos = function(user) {
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
     $.getJSON(apiUrl)
     .done(function(data) {
+      if(jQuery.isEmptyObject(data) === false){
       var container = $("#repos");
       container.html("");
       var reposGroup = $("<ul>").addClass("list-group");
       container.append(reposGroup);
       var reposItem = "";
+      var repoItemDetails = "";
+      reposItemStatus = "";
+      reposStatusIcon = "";
         for(var i = 0; i< data.length; i++){
-          reposItem = $("<li>").addClass("list-group-item m-2 bg-secondary rounded text-white font-weight-bold").append(user + "/" + data[i].name);
+          repoItemDetails = $("<div>").append(user + "/" + data[i].name);
+          reposItemStatus = $("<div>").addClass("status-"+i);
+          reposItem = $("<li>").addClass("list-group-item m-2 bg-secondary rounded text-white font-weight-bold d-flex justify-content-between item-" + i);
           $(".list-group").append(reposItem);
+          $(".item-" + i).append(repoItemDetails);
+          $(".item-" + i).append(reposItemStatus);
+          reposStatusIcon = $("<i>").addClass("fas fa-check-square status-icon icon-success");
+          $(".status-"+i).append(reposStatusIcon);
+          if (data[i].open_issues_count > 0) {
+            $(".status-"+i).html("");
+            reposStatusIcon = $("<i>").addClass("fas fa-times status-icon icon-danger");
+            $(".status-"+i).append(reposStatusIcon);
+            $(".status-"+i).append(data[i].open_issues_count + " issue(s)");
+          }
         }
+      } 
     })
-    .fail(function( jqxhr, textStatus, error ) {
-      var err = textStatus + ", " + error;
-      console.log( "Request Failed: " + err );
+    .fail(function( error ) {
+      alert("user not found");
   });
 };
   
